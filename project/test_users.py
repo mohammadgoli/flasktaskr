b8f930ad95466380fe1a0386f78da6620c2c1f08
Mohammad Goli <gli.mhmd@gmail.com>
2015-08-02 22:57:24 +0430

    latest codes!

--- project/test_users.py
+++ project/test_users.py
@@ -1,59 +1,9 @@
-import os
 import unittest
-
-from views import app, db
-from _config import basedir
+from test_base import TestBase
 from models import Users
+from views import db
 
-TEST_DB = 'test.db'
-
-class TestUsers(unittest.TestCase):
-
-    def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-                                                os.path.join(basedir, TEST_DB)
-        self.app = app.test_client()
-        db.create_all()
-
-    # executed after each test
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    ########################
-    #### helper methods ####
-    ########################
-
-    def login(self, name, password):
-        return self.app.post('/', data=dict(
-            name=name, password=password), follow_redirects=True)
-
-    def register(self, name, email, password, confirm):
-        return self.app.post(
-            'register/',
-            data=dict(name=name, email=email, password=password, confirm=confirm),
-            follow_redirects=True
-        )
-
-    def logout(self):
-        return self.app.get('logout/', follow_redirects=True)
-
-    def create_user(self, name, email, password):
-        new_user = Users(name=name, email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-
-    def create_task(self):
-        return self.app.post('add/', data=dict(
-            name='Go to the bank',
-            due_date='02/05/2015',
-            priority='1',
-            posted_date='02/04/2015',
-            status='1'
-        ), follow_redirects=True)
-
+class TestUsers(TestBase):
 
     def test_users_can_register(self):
         new_user = Users("michael", "michael@mherman.org", "michaelherman")
